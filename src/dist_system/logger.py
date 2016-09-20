dccac2aa97a76b5e5f389202ef606442c9ec8c00
Taeguk Kwon <xornrbboy@gmail.com>
2016-09-21 04:41:47 +0900

    update logger for logging variance parameters.

--- src/dist_system/logger.py
+++ src/dist_system/logger.py
@@ -5,5 +5,5 @@ class Logger(metaclass=SingletonMeta):
     def __init__(self, name):
         self.name = name
 
-    def log(self, msg):
-        print("[{0}] {1}".format(self.name, msg))
+    def log(self, msg, *args):
+        print("[{0}] {1}".format(self.name, msg), *args)
