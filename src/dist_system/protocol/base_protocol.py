b3428484a1c4f371eddad0d8cff56dfa83957f3d
Taeguk Kwon <xornrbboy@gmail.com>
2016-09-21 04:39:41 +0900

    Fix a bug of dictify_from_body in case of list. And generalize and refactor make_msg_data.

--- src/dist_system/protocol/base_protocol.py
+++ src/dist_system/protocol/base_protocol.py
@@ -6,20 +6,6 @@ class BaseProtocol:
         self.protocol = protocol
         self.message_table = msg_table
 
-    def handle_extra(self, data, key, klass):
-        if key == 'task':
-            data[data[key + '_type']] = klass[data[key + '_type']](**data[key])
-
-            data.pop(key + '_type')
-            data.pop(key)
-        elif key == 'result':
-            data[data['task_type']] = klass[data['task_type']](**data[key])
-
-            data.pop('task_type')
-            data.pop(key)
-        else:
-            raise HandleError('%s is not valid key' % key)
-
     def dictify_from_body(self, body):
         field_list = body.ListFields()
         field_dict = dict()
@@ -32,6 +18,8 @@ class BaseProtocol:
                 if desc.containing_oneof:
                     name = desc.containing_oneof.name
                     field_dict[name + '_type'] = desc.name
+            elif hasattr(val, 'sort') and hasattr(val[0], 'ListFields'):    # when list
+                value = list(map(lambda x: self.dictify_from_body(x), val))
 
             field_dict[name] = value
 
@@ -40,27 +28,56 @@ class BaseProtocol:
     # input : string, dict
     # output : bytes
     def make_msg_data(self, header, body):
-        import copy
-        data = copy.deepcopy(body)
 
-        msg_meta = self.message_table.get(header)
+        except_fields = ['task_type']
 
-        if msg_meta is None:
+        def get_adjust_key_name(key, outer_body):
+            if key == 'task':
+                return outer_body['task_type']
+            elif key == 'result':
+                return outer_body['task_type']
+            else:
+                return key
+
+        def handle_exception_case(key, meta, outer_body, outer_data):
+            if key == 'task' or key == 'result':
+                adj_key = get_adjust_key_name(key, outer_body)
+                except_fields.append(key)
+                return meta[adj_key](**outer_body[key])
+            else:
+                raise ValueError
+
+        def make_msg_data_from_body(meta, body):
+            if type(meta) is dict:
+                data = {}
+                for key, inner_meta in meta.items():
+                    if key == 'this':
+                        continue
+                    try:
+                        inner_data = handle_exception_case(key, inner_meta, body, data)
+                    except ValueError:
+                        inner_data = make_msg_data_from_body(inner_meta, body[key])
+                    adj_key = get_adjust_key_name(key, body)
+                    data[adj_key] = inner_data
+            elif type(meta) is list:
+                data = []
+                for inner_body in body:
+                    data.append(make_msg_data_from_body(meta[0], inner_body))
+            else:
+                data = meta(**body)
+            return data
+
+        root_meta = self.message_table.get(header)
+        if root_meta is None:
             raise HeaderError('%s is not valid header' % header)
 
-        for key, klass in msg_meta.items():
-            if key == 'this':
-                msg_klass = klass
-            else:
-                if type(klass) is dict:
-                    self.handle_extra(data, key, klass)
-                elif type(klass) is list:
-                    element_klass = klass[0]
-                    data[key] = list(map(lambda x: element_klass(**x), data[key]))
-                else:
-                    data[key] = klass(**data[key])
+        data = make_msg_data_from_body(root_meta, body)
 
-        msg = {header: msg_klass(**data)}
+        for key, value in body.items():
+            if not key in data and not key in except_fields:
+                data[key] = body[key]
+
+        msg = {header: root_meta['this'](**data)}
         return self.protocol.Message(**msg).SerializeToString()
 
     # input : bytes
