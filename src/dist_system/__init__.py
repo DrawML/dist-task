d2896fd4bb4c142e75d22007e4799104264313bb
Taeguk Kwon <xornrbboy@gmail.com>
2016-10-03 02:31:59 +0900

    Add cloud_dfs as a submodule.

---
+++ src/dist_system/__init__.py
@@ -0,0 +1,4 @@
+import os
+import sys
+os.path.dirname(os.path.abspath(__file__))
+sys.path.append('/home/taeguk/pycharm_remote_ssm/dist-task/src')
---
+++ tests/test_cloud_dfs.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_import(self):
+        import dist_system
+        from cloud_dfs.connector import CloudDFSConnector
+        CloudDFSConnector('127.0.0.1', 5000)
+
+
+if __name__ == '__main__':
+    unittest.main()
