b1b8c41f3d7ee56c72e97b81afcab0bf9e090ad5
Taeguk Kwon <xornrbboy@gmail.com>
2016-10-07 03:13:30 +0900

    Add logging messages in message handler of worker.

--- src/dist_system/worker/msg_handler.py
+++ src/dist_system/worker/msg_handler.py
@@ -1,3 +1,4 @@
+import sys
 import traceback
 
 from dist_system.library import SingletonMeta
@@ -13,24 +14,25 @@ class SlaveMessageHandler(metaclass=SingletonMeta):
         Logger().log("from slave, header={0}, body={1}".format(header, body), level=2)
         try:
             SlaveMessageHandler.__handler_dict[msg_name](self, body)
-        except Exception as e:
-            Logger().log("Unknown Exception occurs! Pass it for continuous running.\n" + traceback.format_exc())
+        except:
+            Logger().log("Unknown Exception occurs! Pass it for continuous running.\n{0}".
+                         format(traceback.format_exc()))
         Logger().log("finish of handling slave message", level=2)
 
     def _h_worker_register_res(self, body):
         status = body['status']
 
         if status == 'fail':
-            import sys
+            Logger().log("WORKER REGISTER REQ FAIL, so I die!")
             sys.exit(0)
 
     def _h_task_cancel_req(self, body):
-        import sys
+        Logger().log("TASK CANCEL REQ, so I die!")
         sys.exit(0)
 
     def _h_task_finish_res(self, body):
         # nothing to do using response message...
-        import sys
+        Logger().log("TASK FINISH RES, so I die!")
         sys.exit(0)
 
     __handler_dict = {
